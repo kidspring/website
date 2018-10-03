a4e6bb8bae70b3ef61c860ab2c14f4f718c1f91d
yillkid <yillkid@gmail.com>
2018-10-03 20:04:15 +0800

    Updates

---
+++ precache-manifest.64c80b35e42a09cd154f3696a7180d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "81c6373dc4eec8decec4",
+    "url": "/website/static/js/main.81c6373d.chunk.js"
+  },
+  {
+    "revision": "c176cc8ce686585b909f",
+    "url": "/website/static/js/1.c176cc8c.chunk.js"
+  },
+  {
+    "revision": "81c6373dc4eec8decec4",
+    "url": "/website/static/css/main.b35d9413.chunk.css"
+  },
+  {
+    "revision": "0fde98d7b1cc928bc2c395629d2cc82a",
+    "url": "/website/index.html"
+  }
+];
